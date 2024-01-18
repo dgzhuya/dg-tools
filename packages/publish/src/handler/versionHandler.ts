e2e1fd4bb958bcc600250588dfce4087f2365d7f
dgzhuya <chenbiuxiu@gmail.com>
2024-01-18 18:28:59 +0800

    ✨ feat: 实现版本号修改功能

--- packages/publish/src/handler/versionHandler.ts
+++ packages/publish/src/handler/versionHandler.ts
@@ -7,36 +7,35 @@ import { formatPkg } from '../utils'
 const luoName = ['特大', '大', '中']
 
 export const versionHandler = async (pkg: PackageInfo) => {
-    const version = pkg.v
-    if (version) {
-        try {
-            const tierVersion = version.split('.').map(i => parseInt(i))
-            const result = await select({
-                message: '请选择更新的版本号',
-                choices: ['大', '中', '小'].map((i, index) => ({
-                    name: pkg.luo ? `${luoName[index]}杯` : `${i}版本`,
-                    value: index,
-                    description: `版本号更新为: ${tierVersion.join('.')}->${updateVersion(tierVersion, index)}`
-                }))
-            })
-            tierVersion[result] += 1
-            const jsonPath = join(pkg.path, 'package.json')
-            const newVersion = tierVersion.join('.')
-            const text = readFileSync(jsonPath, 'utf-8')
-            const data = JSON.parse(text)
-            data.version = newVersion
-            console.log('data: ', data)
-            await formatPkg(JSON.stringify(data), jsonPath)
-            return
-        } catch (error) {
-            throw error
-        }
-    }
-    throw new Error(`${pkg.name}缺少版本信息`)
+	const version = pkg.v
+	if (version) {
+		try {
+			const tierVersion = version.split('.').map(i => parseInt(i))
+			const result = await select({
+				message: '请选择更新的版本号',
+				choices: ['大', '中', '小'].map((i, index) => ({
+					name: pkg.luo ? `${luoName[index]}杯` : `${i}版本`,
+					value: index,
+					description: `版本号更新为: ${tierVersion.join('.')}->${updateVersion(tierVersion, index)}`
+				}))
+			})
+			tierVersion[result] += 1
+			const jsonPath = join(pkg.path, 'package.json')
+			const newVersion = tierVersion.join('.')
+			const text = readFileSync(jsonPath, 'utf-8')
+			const data = JSON.parse(text)
+			data.version = newVersion
+			await formatPkg(JSON.stringify(data, null, 2), jsonPath)
+			return
+		} catch (error) {
+			throw error
+		}
+	}
+	throw new Error(`${pkg.name}缺少版本信息`)
 }
 
 const updateVersion = (list: number[], index = 0, step = 1) => {
-    const newList = [...list]
-    newList[index] += step
-    return newList.join('.')
+	const newList = [...list]
+	newList[index] += step
+	return newList.join('.')
 }
--- packages/publish/src/utils/prettier.ts
+++ packages/publish/src/utils/prettier.ts
@@ -2,8 +2,16 @@ import { format, Options } from 'prettier'
 import { writeFileSync } from 'node:fs'
 
 const defaultConfig: Options = {
+	useTabs: true,
 	tabWidth: 4,
-	useTabs: false
+	printWidth: 80,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	endOfLine: 'crlf',
+	vueIndentScriptAndStyle: true
 }
 
 export const formatPkg = async (
