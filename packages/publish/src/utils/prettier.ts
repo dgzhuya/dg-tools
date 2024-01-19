97b2ce116a751b994520212c887dbcfcee88dc6d
dgzhuya <chenbiuxiu@gmail.com>
2024-01-19 18:13:07 +0800

    🦄 refactor: 清理格式化配置

--- packages/publish/src/utils/prettier.ts
+++ packages/publish/src/utils/prettier.ts
@@ -1,19 +1,6 @@
 import { writeFileSync } from 'node:fs'
 import { format, Options, resolveConfig, resolveConfigFile } from 'prettier'
 
-const defaultConfig: Options = {
-	useTabs: true,
-	tabWidth: 4,
-	printWidth: 80,
-	semi: false,
-	singleQuote: true,
-	trailingComma: 'none',
-	arrowParens: 'avoid',
-	bracketSpacing: true,
-	endOfLine: 'crlf',
-	vueIndentScriptAndStyle: true
-}
-
 export const formatPkg = async (
 	text: string,
 	path: string,
@@ -23,7 +10,6 @@ export const formatPkg = async (
 	const options = file ? await resolveConfig(file) : {}
 
 	const formatted = await format(text, {
-		...defaultConfig,
 		...config,
 		...options,
 		parser: 'json'
