0c70e2f436e224b4c10ce166e49f5123cd10bbb7
dgzhuya <chenbiuxiu@gmail.com>
2024-01-19 17:34:10 +0800

    ✨ feat: 添加包上传动画

--- packages/publish/src/handler/uploadHandler.ts
+++ packages/publish/src/handler/uploadHandler.ts
@@ -1,17 +1,22 @@
 import { input } from '@inquirer/prompts'
-import { execAsync } from '../utils'
+import { execAsync, printLoading } from '../utils'
 import { PackageInfo } from '../option'
 
 export const uploadHandler = async (pkg: PackageInfo, opt?: boolean) => {
 	let command =
 		'npm publish --access=public --registry=https://registry.npmjs.org '
+	let close: Function | undefined
 	try {
 		if (opt) {
 			const otpCode = await input({ message: '请输入单次验证码: ' })
 			command += `--otp=${otpCode}`
 		}
+		close = printLoading('上传中')
 		await execAsync(command, pkg.path)
+		close()
 	} catch (error) {
+		if (close) close()
+
 		throw error
 	}
 }
--- packages/publish/src/utils/log.ts
+++ packages/publish/src/utils/log.ts
@@ -1,12 +1,38 @@
 import chalk from 'chalk'
 
 export const logger = (
-    msg: string | unknown,
-    status: 'success' | 'fail' = 'success'
+	msg: string | unknown,
+	status: 'success' | 'fail' = 'success'
 ) => {
-    if (status === 'success') {
-        console.log(chalk.green(msg))
-    } else {
-        console.log(chalk.red(msg))
-    }
+	if (status === 'success') {
+		console.log(chalk.green(msg))
+	} else {
+		console.log(chalk.red(msg))
+	}
+}
+
+export const printLoading = (msg: string) => {
+	let status: 1 | 2 | 3 | 4 = 1
+	const id = setInterval(() => {
+		console.clear()
+		switch (status) {
+			case 1:
+				logger(`${msg}.`)
+				status = 2
+				break
+			case 2:
+				logger(`${msg}..`)
+				status = 3
+				break
+			case 3:
+				logger(`${msg}...`)
+				status = 4
+				break
+			case 4:
+				logger(`${msg}....`)
+				status = 1
+				break
+		}
+	}, 400)
+	return () => clearInterval(id)
 }
