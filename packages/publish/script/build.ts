34b61fa8b5ec690d7e29de14c95567d80bbbb6fb
dgzhuya <chenbiuxiu@gmail.com>
2024-01-19 16:32:10 +0800

    🐳 chore: 修改编译脚本压缩上传文件

--- packages/publish/script/build.ts
+++ packages/publish/script/build.ts
@@ -3,19 +3,20 @@ import { cwd } from 'process'
 import { build } from 'esbuild'
 
 const buildFile = async (rootPath = cwd()) => {
-    try {
-        await build({
-            bundle: true,
-            banner: {
-                js: '#!/usr/bin/env node'
-            },
-            entryPoints: [join(rootPath, 'src/index.ts')],
-            tsconfig: join(rootPath, 'tsconfig.json'),
-            outfile: join(rootPath, 'dist/index.js'),
-            platform: 'node',
-            external: ['shelljs', '@types/shelljs', 'chalk']
-        })
-    } catch (error) {}
+	try {
+		await build({
+			bundle: true,
+			minify: true,
+			banner: {
+				js: '#!/usr/bin/env node'
+			},
+			entryPoints: [join(rootPath, 'src/index.ts')],
+			tsconfig: join(rootPath, 'tsconfig.json'),
+			outfile: join(rootPath, 'dist/index.js'),
+			platform: 'node',
+			external: ['shelljs', '@types/shelljs', 'chalk', 'esbuild', 'tsx']
+		})
+	} catch (error) {}
 }
 
 buildFile()
