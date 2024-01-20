3d561bff8ce75a3ef633cc84dd1f428a6df128b8
dgzhuya <chenbiuxiu@gmail.com>
2024-01-20 19:35:32 +0800

    🐳 chore: 修改构建脚本

--- packages/publish/script/build.ts
+++ packages/publish/script/build.ts
@@ -3,21 +3,17 @@ import { cwd } from 'process'
 import { build } from 'esbuild'
 import { dependencies } from '../package.json'
 
-const buildFile = async (rootPath = cwd()) => {
-	try {
-		await build({
-			bundle: true,
-			minify: true,
-			banner: {
-				js: '#!/usr/bin/env node'
-			},
-			entryPoints: [join(rootPath, 'src/index.ts')],
-			tsconfig: join(rootPath, 'tsconfig.json'),
-			outfile: join(rootPath, 'dist/index.js'),
-			platform: 'node',
-			external: Object.keys(dependencies)
-		})
-	} catch (error) {}
-}
+const rootPath = cwd()
 
-buildFile()
+build({
+	bundle: true,
+	minify: true,
+	banner: {
+		js: '#!/usr/bin/env node'
+	},
+	entryPoints: [join(rootPath, 'src/index.ts')],
+	tsconfig: join(rootPath, 'tsconfig.json'),
+	outfile: join(rootPath, 'dist/index.js'),
+	platform: 'node',
+	external: Object.keys(dependencies)
+})
