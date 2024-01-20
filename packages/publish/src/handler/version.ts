6997e422f8ef0e846b5d3ac6453abd339ecc92c1
dgzhuya <chenbiuxiu@gmail.com>
2024-01-20 22:20:08 +0800

    🦄 refactor: 添加版本错误信息

--- packages/publish/src/handler/version.ts
+++ packages/publish/src/handler/version.ts
@@ -9,34 +9,34 @@ const luoName = ['特大', '大', '中']
 
 export const versionHandler = async (ctx: XiuContext) => {
 	const version = ctx.pkg?.v
-	if (version) {
-		try {
-			const tierVersion = version.split('.').map(i => parseInt(i))
-			const result = await select({
-				message: '请选择更新的版本号',
-				choices: ['大', '中', '小'].map((i, index) => ({
-					name: ctx.pkg?.luo ? `${luoName[index]}杯` : `${i}版本`,
-					value: index,
-					description: `版本号更新为: ${tierVersion.join('.')}->${updateVersion(tierVersion, index)}`
-				}))
-			})
-			tierVersion[result] += 1
-			if (ctx.pkg) ctx.pkg.v = tierVersion.join('.')
-		} catch (error) {
-			throw new XiuError('21003', 'inquirer-select')
-		}
+	if (!version) throw new XiuError('11004', `${ctx.pkg?.name}`)
 
-		try {
-			const jsonPath = join(ctx.pkg?.path || ctx.cwdPath, 'package.json')
-			const text = readFileSync(jsonPath, 'utf-8')
-			const data = JSON.parse(text)
-			data.version = ctx.pkg?.v
-			await formatPkg(JSON.stringify(data), jsonPath)
-		} catch (error) {
-			throw error
-		}
+	try {
+		const tierVersion = version.split('.').map(i => parseInt(i))
+		const result = await select({
+			message: '请选择更新的版本号',
+			choices: ['大', '中', '小'].map((i, index) => ({
+				name: ctx.pkg?.luo ? `${luoName[index]}杯` : `${i}版本`,
+				value: index,
+				description: `版本号更新为: ${tierVersion.join('.')}->${updateVersion(tierVersion, index)}`
+			}))
+		})
+		tierVersion[result] += 1
+		if (ctx.pkg) ctx.pkg.v = tierVersion.join('.')
+	} catch (error) {
+		throw new XiuError('21003', 'inquirer-select')
 	}
-	throw new Error(`${ctx.pkg?.name}缺少版本信息`)
+
+	try {
+		const jsonPath = join(ctx.pkg?.path || ctx.cwdPath, 'package.json')
+		console.log('jsonPath: ', jsonPath)
+		const text = readFileSync(jsonPath, 'utf-8')
+		const data = JSON.parse(text)
+		data.version = ctx.pkg?.v
+		await formatPkg(JSON.stringify(data), jsonPath)
+	} catch (error) {
+		throw error
+	}
 }
 
 const updateVersion = (list: number[], index = 0, step = 1) => {
