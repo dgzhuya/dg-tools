1fb6d5a13e169e0a02dfefc57ad4947f67451aeb
dgzhuya <chenbiuxiu@gmail.com>
2024-01-20 21:12:52 +0800

    🦄 refactor: 添加prettier错误处理

--- packages/publish/src/utils/prettier.ts
+++ packages/publish/src/utils/prettier.ts
@@ -1,18 +1,23 @@
 import { writeFileSync } from 'node:fs'
 import { format, Options, resolveConfig, resolveConfigFile } from 'prettier'
+import { XiuError } from '../error/xiu-error'
 
 export const formatPkg = async (
 	text: string,
 	path: string,
 	config: Options = {}
 ) => {
-	const file = await resolveConfigFile(path)
-	const options = file ? await resolveConfig(file) : {}
+	try {
+		const file = await resolveConfigFile(path)
+		const options = file ? await resolveConfig(file) : {}
 
-	const formatted = await format(text, {
-		...config,
-		...options,
-		parser: 'json-stringify'
-	})
-	writeFileSync(path, formatted)
+		const formatted = await format(text, {
+			...config,
+			...options,
+			parser: 'json-stringify'
+		})
+		writeFileSync(path, formatted)
+	} catch (error) {
+		throw new XiuError('20000')
+	}
 }
