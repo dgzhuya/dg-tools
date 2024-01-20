9e826d296c9058405a8fdaee1f1944a72cbb4950
dgzhuya <chenbiuxiu@gmail.com>
2024-01-20 22:25:38 +0800

    🐞 fix: hook命令执行目录

--- packages/publish/src/handler/hook.ts
+++ packages/publish/src/handler/hook.ts
@@ -4,12 +4,12 @@ import { execAsync } from '../utils'
 
 export const hookHandler = async (ctx: XiuContext) => {
 	try {
-		if (ctx.build) await execAsync('npm run build', ctx.cwdPath)
+		if (ctx.build) await execAsync('npm run build', ctx.pkg?.path)
 	} catch (error) {
 		throw new XiuError('30004')
 	}
 	try {
-		if (ctx.hook) await execAsync(ctx.hook, ctx.cwdPath)
+		if (ctx.hook) await execAsync(ctx.hook, ctx.pkg?.path)
 	} catch (error) {
 		throw new XiuError('30005')
 	}
