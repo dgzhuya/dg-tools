8ed0db8c45de29d0cacbd2524f00f4042a914857
dgzhuya <chenbiuxiu@gmail.com>
2024-01-24 20:41:50 +0800

    ✨ feat: 添加模板数据读取和渲染

--- packages/template/src/render.ts
+++ packages/template/src/render.ts
@@ -1,6 +1,15 @@
+import { cwd } from 'process'
 import { Kind, Config } from './types'
+import { join } from 'path'
+import { readFile } from 'fs/promises'
+import { Template } from './template'
+import { existsSync } from 'node:fs'
 
 export const renderTemplate = async (name: Kind, config: Config<Kind>) => {
-	console.log(name)
-	console.log(config)
+	const filePath = join(cwd(), 'sources', `${name}.btpl`)
+	if (!existsSync(filePath)) {
+		throw new Error('文件不存在')
+	}
+	const templateStr = await readFile(filePath, 'utf-8')
+	return new Template(templateStr).render(config)
 }
---
+++ packages/template/src/template.ts
@@ -0,0 +1,73 @@
+import { Config, Kind } from './types'
+
+export class Template {
+	#source: string
+	#index: number = 0
+	constructor(source: string) {
+		this.#source = source
+	}
+
+	#peek() {
+		return this.#source[this.#index]
+	}
+
+	#next() {
+		const char = this.#peek()
+		this.#index++
+		return char
+	}
+
+	#hasNext() {
+		return this.#peek() !== undefined
+	}
+
+	#getKey() {
+		let key = ''
+		while (this.#hasNext()) {
+			const char = this.#next()
+			if (char === '%' && this.#peek() === '}') {
+				this.#next()
+				return key
+			}
+			key += char
+		}
+		throw new Error('未匹配到结尾符号')
+	}
+
+	findKeys() {
+		let keys: string[] = []
+		while (this.#hasNext()) {
+			const char = this.#next()
+			if (char === '{' && this.#peek() === '%') {
+				this.#next()
+				const key = this.#getKey()
+				if (!keys.includes(key)) {
+					keys.push(key)
+				}
+			}
+		}
+		return keys
+	}
+
+	render(config: Config<Kind>) {
+		let result = ''
+		while (this.#hasNext()) {
+			const char = this.#next()
+
+			if (char === '%') {
+				console.log(this.#peek())
+			}
+			if (char === '{' && this.#peek() === '%') {
+				this.#next()
+				const val = config[this.#getKey()]
+				if (!val) {
+					throw new Error('传入配置信息错误')
+				}
+				result += val
+			} else {
+				result += char
+			}
+		}
+		return result
+	}
+}
