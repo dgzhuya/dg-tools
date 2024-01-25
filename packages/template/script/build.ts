17a73322a70a7de043198499707016bf338a2151
dgzhuya <chenbiuxiu@gmail.com>
2024-01-25 19:33:50 +0800

    🐳 chore: 修改打包编译输出结果

--- packages/template/script/build.ts
+++ packages/template/script/build.ts
@@ -25,24 +25,26 @@ const getTSFile = (path = join(rootPath, 'src')) => {
 	return entrys
 }
 
-build({
-	bundle: false,
-	entryPoints: getTSFile(),
-	outdir: join(rootPath, 'dist'),
-	tsconfig: join(rootPath, 'tsconfig.json'),
-	plugins: [dtsPlugin()],
-	platform: 'node'
-})
-
-build({
-	bundle: true,
-	minify: true,
-	banner: {
-		js: '#!/usr/bin/env node'
-	},
-	entryPoints: [join(rootPath, 'src/bin/index.ts')],
-	tsconfig: join(rootPath, 'tsconfig.json'),
-	outfile: join(rootPath, 'dist/bin/index.js'),
-	platform: 'node',
-	external: Object.keys(dependencies)
-})
+Promise.all([
+	build({
+		bundle: false,
+		entryPoints: getTSFile(),
+		outdir: join(rootPath, 'dist'),
+		format: 'cjs',
+		tsconfig: join(rootPath, 'tsconfig.json'),
+		plugins: [dtsPlugin()],
+		platform: 'node'
+	}),
+	build({
+		bundle: true,
+		minify: true,
+		banner: {
+			js: '#!/usr/bin/env node'
+		},
+		entryPoints: [join(rootPath, 'src/bin/index.ts')],
+		tsconfig: join(rootPath, 'tsconfig.json'),
+		outfile: join(rootPath, 'dist/bin/index.js'),
+		platform: 'node',
+		external: Object.keys(dependencies)
+	})
+])
