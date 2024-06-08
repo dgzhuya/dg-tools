855d89713b871f890426ee54a8cc067fdc0337d7
dgzhuya <chenbiuxiu@gmail.com>
2024-06-08 22:04:44 +0800

    ✨ feat: 添加同步渲染方法

--- packages/template/src/render.ts
+++ packages/template/src/render.ts
@@ -4,6 +4,7 @@ import { join } from 'path'
 import { readFile } from 'fs/promises'
 import { Template } from './template'
 import { existsSync } from 'node:fs'
+import { readFileSync } from 'fs'
 
 export async function renderTemplate<T extends Kind>(
 	name: T,
@@ -14,5 +15,17 @@ export async function renderTemplate<T extends Kind>(
 		throw new Error('文件不存在')
 	}
 	const templateStr = await readFile(filePath, 'utf-8')
+	return new Template(templateStr).render(config)
+}
+
+export async function renderTemplateSync<T extends Kind>(
+	name: T,
+	config: Config<T>
+) {
+	const filePath = join(cwd(), 'sources', `${name}.btpl`)
+	if (!existsSync(filePath)) {
+		throw new Error('文件不存在')
+	}
+	const templateStr = readFileSync(filePath, 'utf-8')
 	return new Template(templateStr).render(config)
 }
