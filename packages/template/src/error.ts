31f205f56ba1bfcb01aca71def04280ace172302
dgzhuya <chenbiuxiu@gmail.com>
2024-06-25 19:01:56 +0800

    🎈 perf: 优化错误信息展示

--- packages/template/src/error.ts
+++ packages/template/src/error.ts
@@ -1,8 +1,16 @@
 export class XiuError extends Error {
 	message: string
+	key: string
+	funcKey: string
 
-	constructor(msg: string) {
+	constructor(msg: string, key: string, funcKey: string) {
 		super()
 		this.message = msg
+		this.key = key
+		this.funcKey = funcKey
+	}
+
+	show() {
+		return `key:${this.key};func=${this.funcKey};\nmsg=${this.message}`
 	}
 }
