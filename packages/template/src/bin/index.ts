5570962bf7a36c7981cb5c8f512f510c8d519d33
dgzhuya <chenbiuxiu@gmail.com>
2024-06-30 17:10:12 +0800

    ✨ feat: 添加格式化命令

--- packages/template/src/bin/index.ts
+++ packages/template/src/bin/index.ts
@@ -2,9 +2,13 @@ import { BtplEnv } from '..'
 import { join } from 'node:path'
 import { cwd } from 'node:process'
 import { readFile, readdir } from 'node:fs/promises'
+import { FormatParser } from '../template/format'
+import { writeFileSync } from 'node:fs'
 
-const findBtpl = async (path: string) => {
-	const sourcesPath = join(path, 'sources')
+const path = cwd()
+const sourcesPath = join(path, 'sources')
+
+const findBtpl = async () => {
 	const files = await readdir(sourcesPath)
 	const result: { name: string; source: string }[] = []
 	for (const file of files) {
@@ -18,19 +22,27 @@ const findBtpl = async (path: string) => {
 	return result
 }
 
+const [type] = process.argv.slice(2)
+
 const initProject = async () => {
 	try {
-		const path = cwd()
-		const modules = await findBtpl(path)
-
-		const tsFile = join(path, 'btpl-env.d.ts')
-
-		const btpl = new BtplEnv(tsFile)
-		for (const { name, source } of modules) {
-			await btpl.update(name, source)
+		const modules = await findBtpl()
+		if (['--format', '-f'].includes(type)) {
+			for (const { name, source } of modules) {
+				const content = new FormatParser(source).format()
+				writeFileSync(`${join(sourcesPath, name)}.btpl`, content)
+			}
 		}
-		btpl.clear(modules.map(m => m.name))
-		await btpl.save()
+		if (['--key', 'k'].includes(type)) {
+			const tsFile = join(path, 'btpl-env.d.ts')
+
+			const btpl = new BtplEnv(tsFile)
+			for (const { name, source } of modules) {
+				await btpl.update(name, source)
+			}
+			btpl.clear(modules.map(m => m.name))
+			await btpl.save()
+		}
 	} catch (error) {
 		console.log('\x1b[31m', (error as Error).message)
 	}
