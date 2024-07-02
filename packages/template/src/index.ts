83fe831c270e5121b2306d2289cdc19a0b0b2b5f
dgzhuya <chenbiuxiu@gmail.com>
2024-07-02 11:16:09 +0800

    ✨ feat: 导出格式化和代码高亮工具

--- packages/template/src/index.ts
+++ packages/template/src/index.ts
@@ -2,5 +2,8 @@ export * from './render'
 export * from './types'
 export * from './btpl-env'
 export * from './error'
+import { FormatParser } from './template/format'
 import { TypeKeyParser } from './template/key'
-export { TypeKeyParser }
+import { SyntaxParser } from './template/syntax'
+
+export { TypeKeyParser, FormatParser, SyntaxParser }
